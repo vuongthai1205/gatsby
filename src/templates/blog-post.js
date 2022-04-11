bbf009c44d3e236811dfd1eb6166bf405b948f1f
vuongthai1205 <84846136+vuongthai1205@users.noreply.github.com>
2022-04-11 23:03:46 +0700

    11/4/2022 commit 4

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { graphql } from "gatsby";
-import Layout from "../components/layout";
+import Layout from "../components/Layout";
 import {Container, Row } from "react-bootstrap";
 
 const BlogPost = ({ data }) => {
